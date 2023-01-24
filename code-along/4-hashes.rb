32292e4e254274f8aeba69e1b1f93b271a85e03b
xinyiwang99 <xinyiwang2022@u.northwestern.edu>
2023-01-24 02:46:24 +0000

    completehashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,21 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile ={
+    "name" => "Ben",
+    "location" => "Chicago",
+    "status" => "Teaching",
+    "timeline" => [
+        {"status" => "Teaching","time" => "6:00 PM"},
+        {"status" => "driving","time" =>"9:00 PM"}
+    ],
+    :age => "..."
+}
 
 # Accessing data from the hash
+puts profile["name"]
+puts profile["location"]
+puts profile["status"]
+puts profile[:age]
 
 # More Complex Hashes
--- labs/4-hashes.rb
+++ labs/4-hashes.rb
@@ -29,4 +29,13 @@ bitcoin = gets.chomp
 bitcoin = bitcoin.to_f
 
 # 3. inspect the bitcoin_data hash
+bitcoin_value = bitcoin_data
+rate = bitcoin_data["bpi"]["USD"]["rate_float"]
+
+# calculate the value
+value = bitcoin * rate
+
+
 # puts bitcoin_data
+puts " 1 Bitcoin is valued at $#{rate}"
+puts " Your Bitcoin is worth $#{value}"
