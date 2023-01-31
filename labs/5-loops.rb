349ea577185ded13baa47b0d23a963cfe1170c8e
xinyiwang99 <xinyiwang2022@u.northwestern.edu>
2023-01-31 00:19:01 +0000

    completed3

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -9,6 +9,11 @@
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
+for rank in ranks
+    for suit in suits
+        puts "#{rank} of #{suit}"
+    end
+end
 # Sample output:
 # 2 of Clubs
 # 2 of Diamonds
